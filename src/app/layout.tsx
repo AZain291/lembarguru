13ead93646ae002769ff200785924d29bd93e9a1
AZain291 <getmy_z@yahoo.co.id>
2026-06-28 12:40:10 +0700

    seo : search console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
+    verification: {
+    google: 'google-site-verification=0wuqU3shNusJpq6eKK5hriQM3OtT4n6BvdcFiSN_EhQ', // isi dengan kode dari Search Console
+  },
   metadataBase: new URL('https://lembarguru.com'),
     alternates: {
     canonical: '/',
